9afd3c3ebf07324c25c992bda6950d6fe0f03ee6
Sarmad Sabih <sam_sarmad@hotmail.com>
2022-08-25 12:43:29 +0500

    Changes

--- app/javascript/controllers/properties_controller.js
+++ app/javascript/controllers/properties_controller.js
@@ -16,14 +16,14 @@ export default class extends Controller {
         <label style="display: block" for="product_properties_attributes_${index}_name">Name</label>
         <input type="text" name="product[properties_attributes][${index}][name]" id="${idPrefix}_name">
 
-        <label style="display: block" for="product_properties_attributes_#{index}_value">Value</label>
+        <label style="display: block" for="product_properties_attributes_${index}_value">Value</label>
         <input type="text" name="product[properties_attributes][${index}][value]" id="${idPrefix}_value">
         <a data-action="properties#removeProperty" href="javascript:void(0);" data-properties-fieldset-id-param="${fieldSetId}">Remove Property</a>
         <hr>
       </div>
     `
 
-    this.propertyFieldsTarget.innerHTML += html;
+    this.propertyFieldsTarget.insertAdjacentHTML('beforeend', html);
     this.element.dataset.index = Number(index) + 1;
   }
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   has_many :properties, dependent: :destroy
-  accepts_nested_attributes_for :properties, allow_destroy: true
+  accepts_nested_attributes_for :properties, allow_destroy: true, reject_if: :all_blank
 end
